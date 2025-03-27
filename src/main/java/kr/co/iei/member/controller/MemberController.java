119bdc24080ff1d9acb2765145a7b5793e1844a9
JuniPia <junyong0801@naver.com>
2025-03-27 17:57:58 +0900

    feat : 회원가입 기능 완성, 비밀번호 재설정 기능 구현

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,16 +23,31 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	//이메일 중복검사
+	@GetMapping(value="/existsEmail")
+	public ResponseEntity<Integer> existsEmail(@RequestParam String memberEmail){
+		int result = memberService.existsEmail(memberEmail);
+		return ResponseEntity.ok(result);	
+	}
+	
+	//닉네임 중복검사
 	@GetMapping(value="/exists")
 	public ResponseEntity<Integer> exists(@RequestParam String memberNickname){
 		int result = memberService.exists(memberNickname);
 		return ResponseEntity.ok(result);	
 	}
 	
+	//회원가입
 	@PostMapping(value="/join")
 	private ResponseEntity<Integer> join(@RequestBody MemberDTO member){
-		System.out.println(member);
 		int result = memberService.insertMember(member);
-		return ResponseEntity.ok(1);
+		return ResponseEntity.ok(result);
+	}
+	
+	//비밀번호 재설정
+	@PatchMapping(value="/updatrePw")
+	public ResponseEntity<Integer> updatePw(@PathVariable MemberDTO member){
+		int result = memberService.updatePw(member);
+		return ResponseEntity.ok(result);
 	}
 }
--- src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -5,23 +5,49 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.co.iei.member.model.dao.MemberDao;
 import kr.co.iei.member.model.dto.MemberDTO;
+import kr.co.iei.util.JwtUtils;
 
 @Service
 public class MemberService {
 	@Autowired
-	private MemberDao memberDao; 
+	private MemberDao memberDao;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	@Autowired
+	private JwtUtils jwtUtil;
 	
+	//이메일 중복검사
+	public int existsEmail(String memberEmail) {
+		int result = memberDao.existsEmail(memberEmail);
+		return result;
+	}
+	
+	//닉네임 중복검사
 	public int exists(String memberNickname) {
 		int result = memberDao.exists(memberNickname);
 		return result;
 	}
-  
+	
+	//회원가입
+    @Transactional
 	public int insertMember(MemberDTO member) {
-		// TODO Auto-generated method stub
-		return 0;
+		String memberPw = member.getMemberPw();
+		String encPw = encoder.encode(memberPw);
+		member.setMemberPw(encPw);
+		int result = memberDao.insertMember(member);
+		return result;
 	}
+    
+    //비밀번호 재설정
+	public int updatePw(MemberDTO member) {
+		int result = memberDao.updatePw(member);
+		return result;
+	}
+
 }
