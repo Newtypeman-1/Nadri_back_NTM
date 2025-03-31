bda2216a64ed1d877b589727bd3db4e9974f4e6d
leesangminn <tkdals9909@naver.com>
2025-03-31 15:55:21 +0900

    리뷰 관련 페키지 클레스 생성

---
+++ src/main/java/kr/co/iei/review/model/dto/ReviewDTO.java
@@ -0,0 +1,23 @@
+package kr.co.iei.review.model.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Alias(value="review")
+public class ReviewDTO {
+ private int reviewNo;
+ private String memberNickname;
+ private String reviewTitle;
+ private String reviewContnent;
+ private int starRate;
+ private String reviewDate;
+ private int contentId;
+ private int contentTypeId;
+ private int like;
+}
