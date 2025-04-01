e102c7f05a25353244a6f57da8a70d9e6b7fc0c3
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-04-01 19:51:25 +0900

    [feat] 플래너 서버 연동, spotDTO 변경

--- src/main/java/kr/co/iei/tour/model/dto/SpotPlace.java
+++ src/main/java/kr/co/iei/tour/model/dto/SpotPlace.java
@@ -28,4 +28,8 @@ public class SpotPlace {
 	private String usetime;
 	private String restdate;
 	private String parking;
+	
+	//여기서부터는 content 컬럼에 없습니다
+	private int contentRating; //장소에 대한 평점
+	private int contentReview; //장소에 대한 리뷰 수
 }
