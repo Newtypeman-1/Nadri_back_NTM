0bdbe4cde93b796bfc96a079e39923e3001bf1cb
JuniPia <junyong0801@naver.com>
2025-04-02 16:57:14 +0900

    feat : 프로필 사진 업로드, 출력

--- src/main/java/kr/co/iei/WebConfig.java
+++ src/main/java/kr/co/iei/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -15,6 +16,8 @@ import kr.co.iei.chat.model.service.ChatHandler;
 public class WebConfig implements WebMvcConfigurer, WebSocketConfigurer{
   	@Autowired
 	private ChatHandler chat;
+	@Value("${file.root}")
+	private String root;
 	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -23,5 +26,13 @@ public class WebConfig implements WebMvcConfigurer, WebSocketConfigurer{
 	@Bean
 	public BCryptPasswordEncoder bCrypt() {
 		return new BCryptPasswordEncoder();
-	}	
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry
+	.addResourceHandler("/profile/**")
+	.addResourceLocations("file:///"+root+"/profile/");
+	}
 }
+
--- src/main/java/kr/co/iei/member/model/dao/MemberDao.java
+++ src/main/java/kr/co/iei/member/model/dao/MemberDao.java
@@ -1,6 +1,7 @@
 package kr.co.iei.member.model.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.iei.member.model.dto.MemberDTO;
 
@@ -16,4 +17,8 @@ public interface MemberDao {
 	int updatePw(MemberDTO member);
 
 	MemberDTO selectOneMember(String memberEmail);
+
+	MemberDTO selectMemberInfo(String memberNickname);
+
+	int updateMember(MemberDTO member);
 }
