532a69532278ac08c5ebfefc8cdc0413f5060bf2
leesangminn <tkdals9909@naver.com>
2025-04-02 13:27:29 +0900

    feat:프론트에서 axios를 통해 데이터 받기

--- src/main/java/kr/co/iei/review/controller/ReviewController.java
+++ src/main/java/kr/co/iei/review/controller/ReviewController.java
@@ -1,8 +1,13 @@
 package kr.co.iei.review.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.iei.review.model.service.ReviewService;
@@ -13,5 +18,11 @@ import kr.co.iei.review.model.service.ReviewService;
 public class ReviewController {
 	@Autowired
 	private ReviewService reviewService;
+	@GetMapping
+	public ResponseEntity<Map> reviewlist(@RequestParam int reqPage, @RequestParam String value){
+		System.out.println(reqPage);
+		System.out.println(value);
+		return null;
+	}
 
 }
