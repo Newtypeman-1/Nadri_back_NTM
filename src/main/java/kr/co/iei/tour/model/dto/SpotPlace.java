df5c61c63323167136a04727bfc4d115fa04d2ac
Newtypeman-1 <dlfgdjqjfls@gmail.com>
2025-04-04 18:10:17 +0900

    [refactor] spotPlaceDTO 필드 내 변수명 변경

--- src/main/java/kr/co/iei/tour/model/dto/SpotPlace.java
+++ src/main/java/kr/co/iei/tour/model/dto/SpotPlace.java
@@ -11,26 +11,26 @@ import lombok.NoArgsConstructor;
 @Data
 @Alias(value="spot")
 public class SpotPlace {
-	private int contentId;
-	private int contentTypeId;
-	private String contentTitle;
-	private String contentAddr;
-	private String contentTel;
+	private int placeId;
+	private int placeTypeId;
+	private String placeTitle;
+	private String placeAddr;
+	private String placeTel;
 	private String areaCode;
 	private String sigunguCode;
-	private String contentOverview;
-	private String contentCat1;
-	private String contentCat2;
-	private String contentCat3;
+	private String placeOverview;
+	private String placeCat1;
+	private String placeCat2;
+	private String placeCat3;
 	private double mapLat;
 	private double mapLng;
-	private String contentThumb;
+	private String placeThumb;
 	private String usetime;
 	private String restdate;
 	private String parking;
 	
 	//여기서부터는 content 컬럼에 없습니다
-	private int contentRating; //장소에 대한 평점
-	private int contentReview; //장소에 대한 리뷰 수
+	private int placeRating; //장소에 대한 평점
+	private int placeReview; //장소에 대한 리뷰 수
 	private double distance; //마커로부터 장소까지의 거리(m)
 }
