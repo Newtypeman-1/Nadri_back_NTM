46bc2b6aeb38bf0ccd70b8f992dafb18dfe6a9c3
leesangminn <tkdals9909@naver.com>
2025-04-13 16:25:54 +0900

    refactoring:place_type_id로 리뷰리스트 보내고 받기 밑 컨트롤러및 서비스 하나로 합치기

--- src/main/java/kr/co/iei/review/controller/ReviewController.java
+++ src/main/java/kr/co/iei/review/controller/ReviewController.java
@@ -14,13 +14,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import kr.co.iei.admin.model.dto.AdminStatsDTO;
 import kr.co.iei.place.model.dto.PlaceInfoDTO;
+import kr.co.iei.review.model.dto.CommDTO;
 import kr.co.iei.review.model.dto.PlaceImgDTO;
+import kr.co.iei.review.model.dto.ReportDTO;
 import kr.co.iei.review.model.dto.ReviewDTO;
 import kr.co.iei.review.model.service.ReviewService;
 import kr.co.iei.util.FileUtils;
@@ -96,4 +99,64 @@ public class ReviewController {
 		int result = reviewService.updateReview(reviewDTO);
 		return ResponseEntity.ok(result);
 	}
+	@GetMapping(value="/likes/{reviewNo}")
+	public ResponseEntity<Map> reviewLike(@PathVariable int reviewNo){
+		Map map = reviewService.reviewLike(reviewNo);
+		return ResponseEntity.ok(map);
+	
+	}
+	@PostMapping(value="/likes")
+    public ResponseEntity<Integer> addLike(
+            @RequestParam("reviewNo") int reviewNo,
+            @RequestParam("memberNickname") String memberNickname) {
+
+        int result = reviewService.addLike(reviewNo, memberNickname);
+        return ResponseEntity.ok(result); // 성공 시 1, 실패 시 0
+    }   
+	@DeleteMapping("/likes/{reviewNo}")
+    public ResponseEntity<Integer> removeLike(
+            @PathVariable int reviewNo,
+            @RequestBody Map<String, String> body) {
+
+        String memberNickname = body.get("memberNickname");
+        int result = reviewService.removeLike(reviewNo, memberNickname);
+        return ResponseEntity.ok(result); // 성공 시 1, 실패 시 0
+    }
+	@PostMapping(value = "/report/")
+	public ResponseEntity<Integer> insertreport(@RequestBody ReportDTO reportDTO){
+	int result =reviewService.insertReport(reportDTO);
+		 return ResponseEntity.ok(result);
+	}
+	@GetMapping(value="/report/{reviewNo}")
+	public ResponseEntity<List> reportList(@PathVariable int reviewNo){
+		List list= reviewService.reportList(reviewNo);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value="/comm/{reviewNo}")
+	public ResponseEntity<List> commList(@PathVariable int reviewNo){
+	List list = reviewService.commList(reviewNo);
+		 return ResponseEntity.ok(list);
+	}
+	@DeleteMapping(value="/comm/{commNo}")
+	public ResponseEntity<Integer> deleteComm(@PathVariable int commNo){
+		int result = reviewService.deleteComm(commNo);
+		 return ResponseEntity.ok(result);
+	}
+	@PostMapping(value="/comm")
+	public ResponseEntity<CommDTO> insertComm(@ModelAttribute CommDTO comm ){
+		CommDTO comment = reviewService.insertComm(comm);
+		return ResponseEntity.ok(comment);
+	}
+	@PatchMapping("/comm/{commNo}")
+	public ResponseEntity<Integer> patchComment(@PathVariable int commNo,
+			@RequestBody Map<String, String> newComment) {
+		String commContent= newComment.get("commContent");
+		 CommDTO commDTO= new CommDTO();
+		  commDTO.setCommContent(commContent);
+		commDTO.setCommNo(commNo);
+		int result= reviewService.patchComment(commDTO);
+		return ResponseEntity.ok(result);
+	
+}
 }
